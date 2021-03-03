19c0e9aa47dd98dfea5712b82798b12ebe310321
Mubashshir <ahmubashshir@gmail.com>
2021-03-03 12:04:20 +0600

    use pypresence Client instead of Presence and better connection handler.

--- discord-rpc.py
+++ discord-rpc.py
@@ -1,21 +1,24 @@
 import threading
-from pypresence import Presence
+from pypresence import Client
 import time
 import asyncio
 import os
 
 from gi.repository import GObject, Gedit
 
+
 class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
     __gtype_name__ = "ExamplePyWindowActivatable"
 
     window = GObject.property(type=Gedit.Window)
+    _enabled = False
+    _rpc = None
 
     def __init__(self):
         os.chdir(os.path.dirname(__file__))
         GObject.Object.__init__(self)
-        GObject.Object.__init__(self)
-        self.CLIENT_ID = "740171019003756604"
+        self._client_id = "740171019003756604"
+        self._pid = os.getpid()
         self.shall_update = False
         self.langage = ""
         self.name = ""
@@ -37,30 +40,40 @@ class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
             langage = langage.get_name()
 
         name = document.get_uri_for_display()
-        
+
         if (name != self.name or self.langage != langage):
             self.name = name
             self.langage = langage
             self.epoch_start = time.time()
             self.shall_update = True
-    
+
+    def _reconnect(self):
+        if not self._enabled:
+            try:
+                self._rpc = Client(self._client_id)
+                self._rpc.start()
+                self._enabled = True
+            except self._errors:
+                self._enabled = False
+
     def run(self):
         asyncio.set_event_loop(asyncio.new_event_loop())
-        self.RPC = Presence(self.CLIENT_ID)
-        self.RPC.connect()
         while True:
-            if self.shall_update:
-                self.RPC.update(large_image="default", state="Editing " + os.path.basename(self.name), details="Writing " + self.langage + " code", start=self.epoch_start)
+            self._reconnect()
+            if self._enabled and self.shall_update:
+                self._rpc.set_activity(
+                    pid=self._pid,
+                    large_image="default",
+                    details="Writing " + self.langage + " code",
+                    state="Editing " + os.path.basename(self.name),
+                    start=self.epoch_start
+                )
                 self.shall_update = False
-                for i in range(15):
-                    time.sleep(1)
-            else:
-                time.sleep(1)
 
     def do_activate(self):
         self.window.connect("active-tab-changed", self.tab_change)
         self.window.connect("active-tab-state-changed", self.tab_change_state)
-        
+
         self.thread = threading.Thread(target=self.run, daemon=True)
         self.thread.start()
 
@@ -69,4 +82,3 @@ class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
     def do_update_state(self):
         pass
-
