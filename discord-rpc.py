158ff55b7726cc43a5b0fe423faa006b6ccb5969
Mubashshir <ahmubashshir@gmail.com>
2021-04-12 10:10:50 +0600

    use document.props instead of using instance functions.

--- discord-rpc.py
+++ discord-rpc.py
@@ -19,8 +19,7 @@ class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         self._client_id = "740171019003756604"
         self._pid = os.getpid()
         self.shall_update = False
-        self.langage = ""
-        self.name = ""
+        self.props = None
 
     def tab_change_state(self, data):
         document = self.window.get_active_tab().get_document()
@@ -31,18 +30,8 @@ class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         self.update_status(document)
 
     def update_status(self, document):
-        langage = document.get_language()
-
-        if (langage == None):
-            langage = ""
-        else:
-            langage = langage.get_name()
-
-        name = document.get_uri_for_display()
-
-        if (name != self.name or self.langage != langage):
-            self.name = name
-            self.langage = langage
+        if document.props is not self.props:
+            self.props = document.props
             self.epoch_start = time.time()
             self.shall_update = True
 
@@ -63,11 +52,16 @@ class ExamplePyWindowActivatable(GObject.Object, Gedit.WindowActivatable):
                 self._rpc.set_activity(
                     pid=self._pid,
                     large_image="default",
-                    details="Writing " + self.langage + " code",
-                    state="Editing " + os.path.basename(self.name),
+                    details="Writing " + (
+                        self.props.language.get_name()
+                        if self.props.language
+                        else ''
+                    ) + " code",
+                    state="Editing " + self.props.tepl_short_title,
                     start=self.epoch_start
                 )
-                self.shall_update = False
+                if self.props.language:
+                    self.shall_update = False
 
     def do_activate(self):
         self.window.connect("active-tab-changed", self.tab_change)
